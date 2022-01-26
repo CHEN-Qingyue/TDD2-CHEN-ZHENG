0719c3fe4cf1127a72edcf08fd327449ed286a7a
zwhwhwhwh <zzwh990109@gmail.com>
2022-01-26 12:11:01 +0100

    test of func miroir

--- test_tdd2.py
+++ test_tdd2.py
@@ -3,7 +3,7 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    def test_min_int(self):
+    def test_miroir(self):
         self.assertEqual(tdd2.miroir('abcdefg',3),'abcddcba')
         self.assertEqual(tdd2.miroir('miroir',2),'mirrim')
         self.assertEqual(tdd2.miroir('12345',4),'1234554321')
