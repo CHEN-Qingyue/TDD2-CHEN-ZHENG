13fe6e5eed855433ec00fd367778db2f72947422
zwhwhwhwh <zzwh990109@gmail.com>
2022-01-26 21:52:34 +0100

    func derivee2 and derivee_func

--- tdd2.py
+++ tdd2.py
@@ -11,9 +11,29 @@ def miroir(str,index):
 def derivee(listA,t):
     lenth = len(listA)
     if (lenth <=1):
-        return -1
+        return False
     else:
         listB = []
         for i in range(lenth-1):
             listB.append(round((listA[i+1]-listA[i])/t,2))
         return listB
+
+def derivee2(listA,t):
+    lenth = len(listA)
+    if (lenth <=2):
+        return False
+    else:
+        listB = derivee(listA,t)
+        listC = []
+        for i in range(lenth-2):
+            listC.append(round((listB[i+1]-listB[i])/t,2))
+        return listC
+
+
+def derivee_func(func, g, p):
+    if g==1 :
+        dd = 0
+    else :
+        val = str(g)
+        dd = len(val[val.find('.') + 1:])
+    return round(func(p),dd)
